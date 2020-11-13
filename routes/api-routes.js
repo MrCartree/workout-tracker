e5b832b74a51f08186c7429de8052f09912f4d06
Zach LaFleur <zach.lafleur95@gmail.com>
2020-11-13 15:46:16 -0700

    changed a bit of the api/workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,22 +3,30 @@ const Resistance = require("../models/resistance");
 const Cardio = require("../models/cardio");
 
 router.get("/api/workouts", ({ body }, res ) => {
-    Resistance.find({})
-        .sort({ day: -1 })
-        .then(dbResistance => {
-            res.json(dbResistance)
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    Cardio.find({})
-        .sort({ day: -1 })
-        .then(dbCardio => {
-            res.json(dbCardio)
-        })
-        .catch(err => {
-            res.status(400).json(err)
-        });
+
+    Resistance.find({}, (error, data) => {
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(data);
+        }
+    });
+
+    //     .sort({ day: -1 })
+    //     .then(dbResistance => {
+    //         res.json(dbResistance)
+    //     })
+    //     .catch(err => {
+    //         res.status(400).json(err);
+    //     });
+    // Cardio.find({})
+    //     .sort({ day: -1 })
+    //     .then(dbCardio => {
+    //         res.json(dbCardio)
+    //     })
+    //     .catch(err => {
+    //         res.status(400).json(err)
+    //     });
 });
 
 module.exports = router;
